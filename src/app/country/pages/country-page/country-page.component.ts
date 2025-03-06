4fa2fc230637b3cf3264e532a6196add5019b5a5
TitoCalleros <mirna.molina@gmail.com>
2025-03-06 10:12:17 -0600

    Implementación del CountryPageComponent y el detalle.

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, inject, input } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CountryService } from '../../services/country.service';
+import { NotFoundComponent } from "../../../shared/components/not-found/not-found.component";
+import { CountryInformationComponent } from "./country-information/country-information.component";
 
 @Component({
   selector: 'country-page',
-  imports: [],
+  imports: [NotFoundComponent, CountryInformationComponent],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent {
+
+  countryService = inject(CountryService);
+  countryCode = inject(ActivatedRoute).snapshot.params['countryCode'];
+
+  countryResource = rxResource({
+    request: () => ({ countryCode: this.countryCode }),
+    loader: ({ request }) => {
+      return this.countryService.searchCountryByAlphaCode(request.countryCode);
+    }
+  });
 
 }

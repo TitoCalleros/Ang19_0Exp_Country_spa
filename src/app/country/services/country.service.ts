ad96eb12c1a5ca32ee3e77bac8e681b6c1e0855a
TitoCalleros <mirna.molina@gmail.com>
2025-03-06 10:58:19 -0600

    Actualización del valor de retorno de la función searchByCapital

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { catchError, delay, map, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, throwError } from 'rxjs';
 
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { CountryMapper } from '../mappers/country.mapper';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -15,8 +16,10 @@ export class CountryService {
 
   private http = inject(HttpClient);
 
-  searchByCapital( query: string ) {
+  searchByCapital( query: string ):Observable<Country[]> {
     query = query.toLowerCase();
+
+    console.log(`Emitiendo valor: ${ query }`);
 
     return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query }`)
     .pipe(

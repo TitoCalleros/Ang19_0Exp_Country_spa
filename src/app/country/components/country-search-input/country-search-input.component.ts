7f2469562daa256eaa2aeced76a0a80f39a16da8
TitoCalleros <mirna.molina@gmail.com>
2025-03-06 10:59:16 -0600

    Implementación de Debounce (retraso) en el country-search-input component. Con cancelación del mismo.

--- src/app/country/components/country-search-input/country-search-input.component.ts
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -10,6 +10,21 @@ export class CountrySearchInputComponent {
   placeholder = input<string>('Buscar');
 
   searchText = output<string>();
+
+  inputValue = signal<string>('');
+
+  // Efecto que permite esperar un tiempo de 1 segundo antes de emitir el output. La función onClenUp permite cancelar el timeout si hubo otra invocación al efecto.
+  debounceEffect = effect((onCleanUp) => {
+    const value = this.inputValue();
+    const timeout = setTimeout(() => {
+      this.searchText.emit(value);
+    }, 1000);
+
+    onCleanUp( () => {
+      // Función que permite cancelar el timeout
+      clearTimeout(timeout);
+    })
+  } );
 
   onSearch( term: string ): void {
     if ( term.length === 0 ) return;

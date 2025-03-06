409cf93ee29202d51f563f5ed1eea9fb6e5fed99
TitoCalleros <mirna.molina@gmail.com>
2025-03-06 00:33:03 -0600

    Se elimina el delay, el cual se incorporó solo por fines demostrativos.

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { catchError, delay, map, of, throwError } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { CountryMapper } from '../mappers/country.mapper';
@@ -21,7 +21,6 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query }`)
     .pipe(
       map( CountryMapper.mapRestCountryArrayToCountryArray ),
-      delay(3000),
       catchError( () => {
         return throwError(() => new Error('No se pudo obtener países con ese query'))
       })
@@ -34,7 +33,6 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(`${ API_URL }/name/${ query }`)
     .pipe(
       map( CountryMapper.mapRestCountryArrayToCountryArray ),
-      delay(3000),
       catchError( () => {
         return throwError(() => new Error('No se pudo obtener países con ese nombre'))
       })

1e5e6608890542ef71072257f0729020bc61c364
TitoCalleros <mirna.molina@gmail.com>
2025-03-06 22:35:39 -0600

    Ajustes en by-region-page component para mostrar el nuevo componente

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,14 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CountryListComponent } from "../../components/country-list/country-list.component";
-import { Country } from '../../interfaces/country.interface';
+import { CountryRegionComponent } from "../../components/country-region/country-region.component";
+import { CountryService } from '../../services/country.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [CountryListComponent],
+  imports: [CountryListComponent, CountryRegionComponent],
   templateUrl: './by-region-page.component.html',
 })
 export class ByRegionPageComponent {
 
-  countries = signal<Country[]>([]);
+  countryService = inject(CountryService);
+  query = signal('');
+
+  // Implementación con rxResource
+  countryResource = rxResource({
+      request: () => ({ query: this.query() }),
+      loader: ({ request }) => {
+        if (!request.query) return of([])
+
+        return this.countryService.searchCountryByRegion( request.query );
+      }
+    });
+
 
 }
